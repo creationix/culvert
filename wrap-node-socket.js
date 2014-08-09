322d6698405d3fd6d63d270793361e885430327d
Tim Caswell <tim@creationix.com>
2014-08-09 10:32:46 -0500

    Fix node wrapper to handle disconnects better

--- wrap-node-socket.js
+++ wrap-node-socket.js
@@ -3,28 +3,37 @@ var makeChannel = require('./channel');
 module.exports = wrapNodeSocket;
 
 // Given a duplex node stream, return a culvert duplex channel.
-function wrapNodeSocket(socket, options) {
+function wrapNodeSocket(socket, reportError) {
   // Data traveling from node socket to consumer
   var incoming = makeChannel();
   // Data traveling from consumer to node socket
   var outgoing = makeChannel();
 
-  if (options.encoder) outgoing.put = options.encoder(outgoing.put);
-  if (options.decoder) incoming.put = options.decoder(incoming.put);
-
-  var onDrain2 = wrapHandler(onReadable, options.onError);
-  onTake = wrapHandler(onTake, options.onError);
+  var onDrain2 = wrapHandler(onReadable, onError);
+  onTake = wrapHandler(onTake, onError);
 
   var paused = false;
+  var ended = false;
 
   outgoing.take(onTake);
-  if (options.onError) socket.on("error", options.onError);
-  socket.on("drain", wrap(onDrain, options.onError));
-  socket.on("readable", wrap(onReadable, options.onError));
-  socket.on("end", wrap(onEnd, options.onError));
+  socket.on("error", onError);
+  socket.on("drain", wrap(onDrain, onError));
+  socket.on("readable", wrap(onReadable, onError));
+  socket.on("end", wrap(onEnd, onError));
+
+  function onError(err) {
+    if (err.code === "EPIPE" || err.code === "ECONNRESET") {
+      return onEnd();
+    }
+    if (reportError) return reportError(err);
+    console.error(err.stack);
+  }
 
   function onTake(data) {
+    if (ended) return;
+    // console.log("TAKE", data);
     if (data === undefined) {
+      ended = true;
       return socket.end();
     }
     if (socket.write(data)) {
@@ -43,12 +52,16 @@ function wrapNodeSocket(socket, options) {
   }
 
   function onEnd() {
-    incoming.put();
+    if (ended) return;
+    ended = true;
+    incoming.put(undefined);
   }
 
   function onReadable() {
+    if (ended) return;
     while (true) {
       var chunk = socket.read();
+      // console.log("READ", chunk);
       if (!chunk) return;
       if (!incoming.put(chunk)) {
         incoming.drain(onDrain2);
@@ -57,10 +70,28 @@ function wrapNodeSocket(socket, options) {
     }
   }
 
+  var originalOut = outgoing.put;
+  var originalInn = incoming.put;
+
+  function setCodecs() {
+    var out = originalOut;
+    var inn = originalInn;
+    for (var i = 0, l = arguments.length; i < l; ++i) {
+      var codec = arguments[i];
+      if (codec.encoder) out = codec.encoder(out);
+      if (codec.decoder) inn = codec.decoder(inn);
+    }
+    outgoing.put = out;
+    incoming.put = inn;
+  }
+
   return {
-    put: outgoing.put,
+    put: function (item) {
+      return outgoing.put(item);
+    },
     drain: outgoing.drain,
     take: incoming.take,
+    setCodecs: setCodecs
   };
 }
 
